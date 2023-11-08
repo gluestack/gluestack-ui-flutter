ff2d56c6c46d85fe41126ee454bc2c654cf0b370
Alan Paul Varghese <alan@geekyants.com>
2023-11-08 16:54:45 +0530

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:gluestack_flutter_pro/example/hstack_example.dart';
 import 'package:gluestack_flutter_pro/example/image_example.dart';
 import 'package:gluestack_flutter_pro/example/input_example.dart';
 import 'package:gluestack_flutter_pro/example/radio_button_example.dart';
+import 'package:gluestack_flutter_pro/example/text_example.dart';
 import 'package:gluestack_flutter_pro/example/vstack_example.dart';
 
 import 'package:gluestack_flutter_pro/theme_provider.dart';
@@ -35,6 +36,22 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
+                GestureDetector(
+                  onTap: () {
+                    navigatorKey.currentState!.push(MaterialPageRoute(
+                      builder: (context) => const TextExample(),
+                    ));
+                  },
+                  child: const Text(
+                    'GS Text',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+
                GestureDetector(
                   onTap: () {
                     navigatorKey.currentState!.push(MaterialPageRoute(
