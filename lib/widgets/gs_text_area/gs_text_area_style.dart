8abbbb13544fec6379a16c719692581d166dd958
Devanshi Garg <devanshig@geekyants.com>
2023-11-17 11:50:05 +0530

    🎨 refactored textarea

--- lib/widgets/gs_text_area/gs_text_area_style.dart
+++ lib/widgets/gs_text_area/gs_text_area_style.dart
@@ -2,25 +2,6 @@ import 'package:gluestack_flutter_pro/style/gs_style.dart';
 import 'package:gluestack_flutter_pro/theme/config/text_area/text_area.dart';
 
 GSStyle textAreaStyle = GSStyle.fromMap(data: textAreaData);
-// GSStyle baseStyle = GSStyle(
-//   borderWidth: textAreaStyle.borderWidth,
-//   borderColor: textAreaStyle.borderColor,
-//   borderRadius: textAreaStyle.borderRadius,
-//   opacity: textAreaStyle.onDisabled?.opacity,
-//   onHover: textAreaStyle.onHover,
-//   onFocus: textAreaStyle.onFocus,
-// );
-
-// GSStyle compoundVariants = GSStyle(
-//   variants: Variants(
-//     variant: GSVariant(
-//       underlined: baseStyle.merge(inputStyle.variants?.variant?.underlined),
-//       outline: baseStyle.merge(inputStyle.variants?.variant?.outline),
-//       rounded: baseStyle.merge(inputStyle.variants?.variant?.rounded),
-//     ),
-//     size: inputStyle.variants?.size,
-//   ),
-// );
 
 class GSTextAreaStyle {
   static Map<GSSizes, GSStyle> size = {
