6b3b60df64dc0b4b632c199e467f54eb3beb1096
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2023-11-22 19:51:03 +0530

    improved example

--- lib/example/avatar_example.dart
+++ lib/example/avatar_example.dart
@@ -18,23 +18,23 @@ class AvatarExample extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            // GSAvatar(
-            //   radius: GSBorderRadius.$xl,
-            //   size: GSSizes.$md,
-            //   style: GSStyle(bg: Colors.yellow),
-            //   avatarImage: const GSImage(
-            //     imageType: GSImageType.asset,
-            //     path: "assets/images/button.png",
-            //   ),
-            //   fallBackText: GSAvatarFallBackText(
-            //     'KS',
-            //     overflow: TextOverflow.ellipsis,
-            //     style: GSStyle(
-            //       textStyle: const TextStyle(color: $GSColors.pink200),
-            //     ),
-            //   ),
-            //   avatarBadge: const GSAvatarBadge(),
-            // ),
+            GSAvatar(
+              radius: GSBorderRadius.$xl,
+              size: GSSizes.$md,
+              style: GSStyle(bg: Colors.yellow),
+              avatarImage: const GSImage(
+                imageType: GSImageType.asset,
+                path: "assets/images/button.png",
+              ),
+              fallBackText: GSAvatarFallBackText(
+                'KS',
+                overflow: TextOverflow.ellipsis,
+                style: GSStyle(
+                  textStyle: const TextStyle(color: $GSColors.pink200),
+                ),
+              ),
+              avatarBadge: const GSAvatarBadge(),
+            ),
             GSAvatar(
               size: GSSizes.$2xl,
               style: GSStyle(
@@ -53,60 +53,60 @@ class AvatarExample extends StatelessWidget {
                 'Geeky Stack',
               ),
             ),
-            // GSAvatar(
-            //   style: GSStyle(
-            //     bg: Colors.blueAccent,
-            //     textStyle: const TextStyle(color: Colors.white),
-            //   ),
-            //   avatarImage: const GSImage(
-            //     imageType: GSImageType.network,
-            //     path: "https://placehold.co/300x300/png",
-            //   ),
-            //   fallBackText: const GSAvatarFallBackText(
-            //     'Flutter',
-            //   ),
-            // ),
-            // GSAvatarGroup(
-            //   style: GSStyle(gap: -25),
-            //   children: [
-            //     GSAvatar(
-            //       size: GSSizes.$sm,
-            //       style: GSStyle(
-            //         bg: Colors.orange,
-            //         textStyle: const TextStyle(color: Colors.white),
-            //       ),
-            //       avatarBadge: GSAvatarBadge(
-            //         style: GSStyle(
-            //           bg: Colors.red,
-            //           borderRadius: 12,
-            //           borderColor: Colors.yellow,
-            //         ),
-            //       ),
-            //       fallBackText: const GSAvatarFallBackText(
-            //         'Geeky Stack',
-            //       ),
-            //     ),
-            //     GSAvatar(
-            //       size: GSSizes.$sm,
-            //       style: GSStyle(
-            //         bg: Colors.red,
-            //         textStyle: const TextStyle(color: Colors.white),
-            //       ),
-            //       avatarBadge: GSAvatarBadge(
-            //         left: 0,
-            //         bottom: 0,
-            //         style: GSStyle(
-            //           borderWidth: 1,
-            //           borderRadius: 12,
-            //           borderColor: Colors.yellow,
-            //         ),
-            //       ),
-            //       fallBackText: const GSAvatarFallBackText(
-            //         'Geeky Stack',
-            //       ),
-            //     ),
-            //   ],
-            // ),
+            GSAvatar(
+              style: GSStyle(
+                bg: Colors.blueAccent,
+                textStyle: const TextStyle(color: Colors.white),
+              ),
+              avatarImage: const GSImage(
+                imageType: GSImageType.network,
+                path: "https://placehold.co/300x300/png",
+              ),
+              fallBackText: const GSAvatarFallBackText(
+                'Flutter',
+              ),
+            ),
+            GSAvatarGroup(
+              style: GSStyle(gap: -25),
+              children: [
+                GSAvatar(
+                  size: GSSizes.$sm,
+                  style: GSStyle(
+                    bg: Colors.orange,
+                    textStyle: const TextStyle(color: Colors.white),
+                  ),
+                  avatarBadge: GSAvatarBadge(
+                    style: GSStyle(
+                      bg: Colors.red,
+                      borderRadius: 12,
+                      borderColor: Colors.yellow,
+                    ),
+                  ),
+                  fallBackText: const GSAvatarFallBackText(
+                    'Geeky Stack',
+                  ),
+                ),
+                GSAvatar(
+                  size: GSSizes.$sm,
+                  style: GSStyle(
+                    bg: Colors.red,
+                    textStyle: const TextStyle(color: Colors.white),
+                  ),
+                  avatarBadge: GSAvatarBadge(
+                    left: 0,
+                    bottom: 0,
+                    style: GSStyle(
+                      borderWidth: 1,
+                      borderRadius: 12,
+                      borderColor: Colors.yellow,
+                    ),
+                  ),
+                  fallBackText: const GSAvatarFallBackText(
+                    'Geeky Stack',
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
