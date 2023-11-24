b4a16bbe94e5973349d3e2f0bfde49283ad33088
Alan Paul Varghese <alan@geekyants.com>
2023-11-24 12:41:51 +0530

    added KSBookingOptions

--- lib/example/KitchenSink/components/sidebar/sidebar.dart
+++ lib/example/KitchenSink/components/sidebar/sidebar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gluestack_flutter_pro/example/KitchenSink/components/sidebar/sortby_checkbox.dart';
+import 'package:gluestack_flutter_pro/example/KitchenSink/components/sidebar/booking_options.dart';
+import 'package:gluestack_flutter_pro/example/KitchenSink/components/sidebar/sortby.dart';
+import 'package:gluestack_flutter_pro/style/gs_style.dart';
 import 'package:gluestack_flutter_pro/widgets/gs_vstack/gs_vstack.dart';
 
 class KSSideBar extends StatelessWidget {
@@ -8,9 +10,11 @@ class KSSideBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GSVStack(
+      space: GSSpaces.$lg,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         KSSortBy(),
+        KSBookingOptions(),
       ],
     );
   }
