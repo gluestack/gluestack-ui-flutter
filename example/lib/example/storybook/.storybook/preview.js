c697a2d24c9dc1d1193f3d48c13c38b90da2f779
Devanshi Garg <devanshig@geekyants.com>
2023-11-30 13:28:04 +0530

    🎉 initial set up docs

---
+++ example/lib/example/storybook/.storybook/preview.js
@@ -0,0 +1,92 @@
+import { addParameters } from '@storybook/client-api';
+import { DocsContainer } from '@storybook/addon-docs/blocks';
+import { config } from '@gluestack-ui/config';
+import { Center, GluestackUIProvider } from '@gluestack-ui/themed';
+import { useState } from 'react';
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    // inlineStories: false,
+  },
+  options: {
+    storySort: {
+      method: '',
+      order: [
+        'Overview',
+        ['Introduction'],
+      ],
+    },
+  },
+};
+
+import { useDarkMode } from '../src/hooks/useDarkMode';
+import { Platform } from 'react-native';
+
+export const decorators = [
+  (Story) => {
+    let value = false;
+
+    if (Platform.OS === 'web') {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      value = useDarkMode();
+    }
+    const [isDark] = useState(false);
+
+    function getColorMode() {
+      //@ts-ignore
+      if (Platform.OS === 'web') {
+        return value ? 'dark' : 'light';
+      } else {
+        return isDark ? 'dark' : 'light';
+      }
+    }
+    return (
+      <GluestackUIProvider
+        config={config}
+        // colorMode={getColorMode()}
+        // components={components}
+      >
+        <Center>
+          <Story />
+        </Center>
+      </GluestackUIProvider>
+    );
+  },
+];
+
+addParameters({
+  docs: {
+    container: ({ children, context }) => {
+      let value = false;
+
+      if (Platform.OS === 'web') {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        value = useDarkMode();
+      }
+      const [isDark] = useState(false);
+
+      function getColorMode() {
+        //@ts-ignore
+        if (Platform.OS === 'web') {
+          return value ? 'dark' : 'light';
+        } else {
+          return isDark ? 'dark' : 'light';
+        }
+      }
+
+      return (
+        <DocsContainer context={context}>
+          <GluestackUIProvider config={config} colorMode={getColorMode()}>
+            {children}
+          </GluestackUIProvider>
+        </DocsContainer>
+      );
+    },
+  },
+});
---
+++ example/lib/example/storybook/babel.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+console.log('herher');
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      process.env.NODE_ENV !== 'production'
+        ? [
+            'module-resolver',
+            {
+              alias: {
+                // '@gluestack-ui/themed': path.join(
+                //   __dirname,
+                //   '../../packages/themed/src'
+                // ),
+                // '@gluestack-ui/config': path.join(
+                //   __dirname,
+                //   '../../packages/config/src/gluestack-ui.config'
+                // ),
+                // '@gluestack-style/react': path.join(
+                //   __dirname,
+                //   '../../../dank-style/packages/react/src'
+                // ),
+                // '@gluestack-style/animation-resolver': path.join(
+                //   __dirname,
+                //   '../../../dank-style/packages/animation-resolver/src'
+                // ),
+                // '@gluestack-style/legend-motion-animation-driver': path.join(
+                //   __dirname,
+                //   '../../../dank-style/packages/animation-legend-motion-driver/src'
+                // ),
+              },
+            },
+          ]
+        : [
+            'babel-plugin-react-docgen-typescript',
+            {
+              exclude: 'node_modules',
+            },
+          ],
+      '@babel/plugin-transform-modules-commonjs',
+    ],
+  };
+};
