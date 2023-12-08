49f0b3e9827aaa6775ca59e7495e8191eb601f56
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2023-12-08 11:33:16 +0530

    made a common rowcol resp widget

---
+++ example/lib/example/KitchenSink/responsive_base_widgets/resp_rowcol_common.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:gluestack_ui/gluestack_ui.dart';
+import 'package:gluestack_ui/src/style/style_resolver.dart';
+
+class RespRowColumnCommon extends StatelessWidget {
+  final double? constraintWidth;
+  final List<Widget> children;
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+  final TextDirection? textDirection;
+  final VerticalDirection verticalDirection;
+  final TextBaseline? textBaseline;
+  final Clip clipBehavior;
+  final Axis? direction;
+  final EdgeInsets? seperatorPadding;
+
+  const RespRowColumnCommon({
+    super.key,
+    required this.children,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.textDirection,
+    this.verticalDirection = VerticalDirection.down,
+    this.textBaseline,
+    this.clipBehavior = Clip.none,
+    this.direction,
+    this.seperatorPadding,
+    this.constraintWidth = 768,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    bool isSmallScreen = isScreenSmallerThan(constraintWidth!, context);
+
+    return Flex(
+      mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment:
+          isSmallScreen ? CrossAxisAlignment.stretch : crossAxisAlignment,
+      clipBehavior: clipBehavior,
+      mainAxisSize: mainAxisSize,
+      textBaseline: textBaseline,
+      textDirection: textDirection,
+      verticalDirection: verticalDirection,
+      direction: direction ?? (isSmallScreen ? Axis.vertical : Axis.horizontal),
+      children: children.map((e) {
+        if (!isSmallScreen) {
+          return GSBox(
+              style: GSStyle(
+                padding: seperatorPadding ??
+                    const EdgeInsets.symmetric(horizontal: 4),
+              ),
+              child: e);
+        } else {
+          return GSBox(
+                style: GSStyle(
+                  padding: seperatorPadding ??
+                      const EdgeInsets.symmetric(vertical: 4),
+                ),
+                child: e);
+        }
+      }).toList(),
+    );
+  }
+}
