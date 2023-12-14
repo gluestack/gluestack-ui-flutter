4af9ff1ebf2844a35b491325c15ddc5df993bd98
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2023-12-14 12:17:08 +0530

    modified breakpoint names to web ones

--- lib/src/token/screen_breakpoint.dart
+++ lib/src/token/screen_breakpoint.dart
@@ -4,25 +4,25 @@ var screenBreakpointInstance = getIt<GSScreenBreakpointToken>();
 
 class GSScreenBreakpointToken {
   final double base;
-  final double small; //sm
-  final double medium; //md
-  final double large; //lg
-  final double extraLarge; //xxl
+  final double sm; //small
+  final double md; //medium
+  final double lg; //large
+  final double xxl; //extraLarge
 
   const GSScreenBreakpointToken({
     this.base = 0,
-    this.small = 480,
-    this.medium = 786,
-    this.large = 992,
-    this.extraLarge = 1280,
+    this.sm = 480,
+    this.md = 786,
+    this.lg = 992,
+    this.xxl = 1280,
   });
 }
 
 class $GSScreenBreakpoint {
   static get base => screenBreakpointInstance.base;
-  static get small => screenBreakpointInstance.small;
-  static get medium => screenBreakpointInstance.medium;
-  static get large => screenBreakpointInstance.large;
-  static get extraLarge => screenBreakpointInstance.extraLarge;
+  static get small => screenBreakpointInstance.sm;
+  static get medium => screenBreakpointInstance.md;
+  static get large => screenBreakpointInstance.lg;
+  static get extraLarge => screenBreakpointInstance.xxl;
 
 }
