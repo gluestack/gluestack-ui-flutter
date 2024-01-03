7c543aa787185e260893b3adfa368a6b6167979b
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-01-03 18:26:14 +0530

    added test cases

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,10 +7,163 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gluestack_ui/gluestack_ui.dart';
 
 import 'package:gluestack_ui_example/main.dart';
 
 void main() {
+  Widget testFrame(Widget child) {
+    return GluestackProvider(
+      child: MaterialApp(
+        home: child,
+      ),
+    );
+  }
+
+//============= GS BUTTON TEST CASES ==============================
+
+//Test: Test single button tap
+  testWidgets('Test button tap', (WidgetTester tester) async {
+    bool tapped = false;
+
+    // Build the widget
+    await tester.pumpWidget(
+      testFrame(
+        GSButton(
+          child: const GSText(
+            text: 'GSButton',
+          ),
+          onPressed: () {
+            tapped = true;
+          },
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('GSButton'));
+    await tester.pump();
+
+    expect(tapped, true);
+  });
+
+//Test: Test double button tap
+  testWidgets('Test double button tap', (WidgetTester tester) async {
+    bool doubleTapped = false;
+    int count = 0;
+
+    // Build the widget
+    await tester.pumpWidget(
+      testFrame(
+        GSButton(
+          child: const GSText(
+            text: 'GSButton',
+          ),
+          onPressed: () {
+            count++;
+            if (count == 2) {
+              doubleTapped = true;
+            }
+          },
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('GSButton'));
+    await tester.tap(find.text('GSButton'));
+    await tester.pump();
+
+    expect(doubleTapped, true);
+  });
+
+//Test: Test long press
+  testWidgets('Test long press', (WidgetTester tester) async {
+    bool longPressed = false;
+
+    // Build the widget
+    await tester.pumpWidget(
+      testFrame(
+        GSButton(
+          child: const GSText(
+            text: 'GSButton',
+          ),
+          onPressed: (){},
+          onLongPress: () {
+            longPressed = true;
+          },
+        ),
+      ),
+    );
+
+    await tester.longPress(find.text('GSButton'));
+    await tester.pump();
+
+    expect(longPressed, true);
+  });
+
+//Test: Test long press
+  testWidgets('Test long press', (WidgetTester tester) async {
+    bool longPressed = false;
+
+    // Build the widget
+    await tester.pumpWidget(
+      testFrame(
+        GSButton(
+          child: const GSText(
+            text: 'GSButton',
+          ),
+          onPressed: (){},
+          onLongPress: () {
+            longPressed = true;
+          },
+        ),
+      ),
+    );
+
+    await tester.longPress(find.text('GSButton'));
+    await tester.pump();
+
+    expect(longPressed, true);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
@@ -18,8 +171,8 @@ void main() {
     // Verify that platform version is retrieved.
     expect(
       find.byWidgetPredicate(
-        (Widget widget) => widget is Text &&
-                           widget.data!.startsWith('Running on:'),
+        (Widget widget) =>
+            widget is Text && widget.data!.startsWith('Running on:'),
       ),
       findsOneWidget,
     );
