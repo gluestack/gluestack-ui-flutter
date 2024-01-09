63bc9f48ac90002b59e39bbac6f46a13a4fe44ae
Manuinder Sekhon <manuinder@geekyants.com>
2024-01-09 20:18:57 +0530

    feat: use riverpod to manage theme state throughout the app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gluestack_ui/gluestack_ui.dart';
 import 'package:gluestack_ui_example/custom_config.dart';
+import 'package:gluestack_ui_example/providers/theme_provider/theme_provider.dart';
 import 'package:gluestack_ui_example/routes/router.dart';
 
-// ignore: library_private_types_in_public_api
-GlobalKey<_MyAppState> myAppStateKey = GlobalKey<_MyAppState>();
 void main() {
-  runApp(MyApp(
-    key: myAppStateKey,
-  ));
+  runApp(const ProviderScope(child: MyApp()));
 }
-
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -21,39 +17,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // Currently selected theme mode
-  ThemeMode _selectedThemeMode = ThemeMode.light;
-
-  void toggleThemeMode() {
-    setState(() {
-      _selectedThemeMode = switch (_selectedThemeMode) {
-        ThemeMode.light => ThemeMode.dark,
-        ThemeMode.dark || ThemeMode.system => ThemeMode.light,
-      };
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return GluestackProvider(
-      gluestackCustomConfig: GluestackCustomConfig(
-        button: customButton,
-        buttonText: customButtonText,
-      ),
-      child: MaterialApp.router(
-        routerConfig: router,
-        debugShowCheckedModeBanner: false,
-        themeMode: _selectedThemeMode,
-        theme: ThemeData(
-          colorSchemeSeed: Colors.deepPurple,
-          useMaterial3: true,
-          brightness: Brightness.light,
-        ),
-        darkTheme: ThemeData(
-          colorSchemeSeed: Colors.deepPurple,
-          useMaterial3: true,
-          brightness: Brightness.dark,
-        ),
+      // This is how we can provide custom configuration to Gluestack Theme.
+      // gluestackCustomConfig: GluestackCustomConfig(
+      //   button: customButton,
+      //   buttonText: customButtonText,
+      // ),
+      child: Consumer(
+        builder: (context, ref, child) {
+          final currentThemeMode = ref.watch(toggleThemeProvider);
+
+          return MaterialApp.router(
+            routerConfig: router,
+            themeMode: currentThemeMode,
+            theme: ThemeData(
+                colorSchemeSeed: Colors.deepPurple,
+                useMaterial3: true,
+                brightness: Brightness.light,
+                appBarTheme:
+                    Theme.of(context).appBarTheme.copyWith(elevation: 4.0)),
+            darkTheme: ThemeData(
+                colorSchemeSeed: Colors.deepPurple,
+                useMaterial3: true,
+                brightness: Brightness.dark,
+                appBarTheme:
+                    Theme.of(context).appBarTheme.copyWith(elevation: 4.0)),
+          );
+        },
       ),
     );
   }
