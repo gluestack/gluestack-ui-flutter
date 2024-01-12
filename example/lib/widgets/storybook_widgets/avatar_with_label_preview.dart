207db9a8858bb22f0a27bed03d538c475c897dc1
ashishg <ashishg@geekyants.com>
2024-01-12 18:41:02 +0530

    fix: routing changes

---
+++ example/lib/widgets/storybook_widgets/avatar_with_label_preview.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:gluestack_ui/gluestack_ui.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
+
+class AvatarWithLabelPreview extends StatelessWidget {
+  const AvatarWithLabelPreview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return materialWrapper(
+      context,
+      Storybook(
+        initialStory: 'Avatar',
+        stories: [
+          Story(
+            name: 'Avatar',
+            builder: (context) => GSVStack(
+              children: [
+                GSHStack(
+                  children: [
+                    GSAvatar(
+                      style: GSStyle(
+                        bg: Colors.orange,
+                        textStyle: const TextStyle(color: Colors.white),
+                      ),
+                      fallBackText: const GSAvatarFallBackText('Geeky Stack'),
+                    ),
+                    const GSHeading(text: 'text')
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
