14c92994ec51f98dedfb9c128c4aad9629dfafff
ashishg <ashishg@geekyants.com>
2024-02-12 16:24:44 +0530

    Copy to code added to iframe

--- example/lib/example/storybook/src/IframeModule.tsx
+++ example/lib/example/storybook/src/IframeModule.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import { MdContentCopy } from "react-icons/md";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 interface IframeProps {
   src: string;
@@ -10,13 +12,25 @@ interface IframeProps {
 }
 
 const IframeModule: React.FC<IframeProps> = ({ src, title, height, code }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div
-      className="ButtonPreview"
+      className="IframeModule"
       style={{
         border: "0.3px solid #404040",
         borderRadius: "0 0 12px 12px ",
         padding: "1px",
+        position: "relative",
       }}
     >
       <iframe
@@ -24,8 +38,23 @@ const IframeModule: React.FC<IframeProps> = ({ src, title, height, code }) => {
         title={title}
         width="100%"
         height="400"
-        style={{ border: "none", padding: "none", marginBottom: "-20px" }} // Adjust margin to remove the gap
+        style={{ border: "none", padding: "none", marginBottom: "-20px" }}
       ></iframe>
+      <div
+        style={{
+          position: "absolute",
+          top: "20px",
+          right: "20px",
+          cursor: "pointer",
+          color: isHovered ? "#fff" : "#333",
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <CopyToClipboard text={code} onCopy={handleCopy}>
+          <MdContentCopy size={24} />
+        </CopyToClipboard>
+      </div>
       <SyntaxHighlighter
         language="dart"
         style={darcula}
@@ -37,6 +66,22 @@ const IframeModule: React.FC<IframeProps> = ({ src, title, height, code }) => {
       >
         {code}
       </SyntaxHighlighter>
+      {copied && (
+        <div
+          style={{
+            position: "absolute",
+            top: "55px",
+            right: "10px",
+            backgroundColor: "rgba(255, 255, 255, 0.9)",
+            padding: "5px",
+            borderRadius: "5px",
+            boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
+            zIndex: 1,
+          }}
+        >
+          Copied!
+        </div>
+      )}
     </div>
   );
 };
