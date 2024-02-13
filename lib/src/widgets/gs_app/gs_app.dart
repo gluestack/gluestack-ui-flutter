b318ad6da742006b444c50ea8eed8fee3568bd52
ashishg <ashishg@geekyants.com>
2024-02-13 12:28:04 +0530

    fix: removed storybook code from flutter core code and move it to root.

--- lib/src/widgets/gs_app/gs_app.dart
+++ lib/src/widgets/gs_app/gs_app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 class GSApp extends WidgetsApp {
