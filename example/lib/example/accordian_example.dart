2f1b01417decb4ea3e63cee9de82178ca491d3cd
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-02-14 13:35:32 +0530

    updated accordion example

--- example/lib/example/accordian_example.dart
+++ example/lib/example/accordian_example.dart
@@ -1,9 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:gluestack_ui/gluestack_ui.dart';
 import 'package:gluestack_ui_example/widgets/base_layout.dart';
+import 'package:gluestack_ui_example/widgets/drop_down.dart';
+import 'package:gluestack_ui_example/widgets/toggle.dart';
 
-class AccordionExample extends StatelessWidget {
+class AccordionExample extends StatefulWidget {
   const AccordionExample({super.key});
+
+  @override
+  State<AccordionExample> createState() => _AccordionExampleState();
+}
+
+class _AccordionExampleState extends State<AccordionExample> {
+  final List dropdownSizeOptions = [
+    GSAccordionSizes.$sm,
+    GSAccordionSizes.$md,
+    GSAccordionSizes.$lg
+  ];
+  GSAccordionSizes selectedSizeOption = GSAccordionSizes.$md;
+
+  final List dropdownTypeOptions = [
+    GSAccordionTypes.single,
+    GSAccordionTypes.multiple,
+  ];
+  GSAccordionTypes selectedTypeOption = GSAccordionTypes.multiple;
+
+  final List dropdownVariantOptions = [
+    GSAccordionVariants.filled,
+    GSAccordionVariants.unfilled,
+  ];
+  GSAccordionVariants selectedVariantOption = GSAccordionVariants.unfilled;
+
+  bool isDisabled = false;
+  bool isCollapsible = false;
+
+  void updateSizeSelectedOption(dynamic newOption) {
+    setState(() {
+      selectedSizeOption = newOption;
+    });
+  }
+
+  void updateVariantSelectedOption(dynamic newOption) {
+    setState(() {
+      selectedVariantOption = newOption;
+    });
+  }
+
+  void updateTypeSelectedOption(dynamic newOption) {
+    setState(() {
+      selectedTypeOption = newOption;
+    });
+  }
+
+  void updateIsDisabled(bool value) {
+    setState(() {
+      isDisabled = value;
+    });
+  }
+
+  void updateIsCollapsible(bool value) {
+    setState(() {
+      isCollapsible = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,27 +95,66 @@ GSAccordion(
       ),
       body: BaseLayout(
         code: code,
-        component: const GSAccordion(
-          animationDuration: Duration(milliseconds: 200),
-          // gsAccordionSize: GSAccordionSizes.$md,
-          // gsAccordionType: GSAccordionTypes.multiple,
-          // gsAccordionVariant: GSAccordionVariants.filled,
-          isCollapsible: true,
-          // iconWhenTileCollapsed: GSAccordionIcon(icon: Icons.abc),
-          isDisabled: false,
+        component: GSAccordion(
+          gsAccordionSize: selectedSizeOption,
+          gsAccordionVariant: selectedVariantOption,
+          gsAccordionType: selectedTypeOption,
+          isCollapsible: isCollapsible,
+          isDisabled: isDisabled,
+          children: const [
+            GSAccordionItem(
+              title: GSAccordionTitle(text: 'How do I place an order?'),
+              content: GSAccordionContent(
+                text:
+                    "To place an order, simply select the products you want, proceed to checkout, provide shipping and payment information, and finalize your purchase.",
+              ),
+            ),
+            GSAccordionItem(
+              title:
+                  GSAccordionTitle(text: 'What payment options do you accept?'),
+              content: GSAccordionContent(
+                text:
+                    "We accept all major credit cards, including Visa, Mastercard, and American Express. We also support payments through PayPal.",
+              ),
+            ),
+          ],
+        ),
+        controls: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            GSAccordionItem(
-              title: GSAccordionTitle(text: 'Title'),
-              content: GSAccordionContent(
-                text: "Content Bigggggg ",
-              ),
+            CustomDropDown(
+              title: "size",
+              dropdownOptions: dropdownSizeOptions,
+              selectedOption: selectedSizeOption,
+              onChanged: updateSizeSelectedOption,
             ),
-            GSAccordionItem(
-              title: GSAccordionTitle(text: 'Title 2'),
-              content: GSAccordionContent(
-                text: "Content 2",
-              ),
+            const SizedBox(height: 20),
+            CustomDropDown(
+              title: "variant",
+              dropdownOptions: dropdownVariantOptions,
+              selectedOption: selectedVariantOption,
+              onChanged: updateVariantSelectedOption,
             ),
+            const SizedBox(height: 20),
+            CustomDropDown(
+              title: "action",
+              dropdownOptions: dropdownTypeOptions,
+              selectedOption: selectedTypeOption,
+              onChanged: updateTypeSelectedOption,
+            ),
+            const SizedBox(height: 20),
+            CustomToggle(
+              title: "isDisabled",
+              value: isDisabled,
+              onToggle: updateIsDisabled,
+            ),
+            const SizedBox(height: 20),
+            CustomToggle(
+              title: "isCollapsible",
+              value: isCollapsible,
+              onToggle: updateIsCollapsible,
+            )
           ],
         ),
       ),
