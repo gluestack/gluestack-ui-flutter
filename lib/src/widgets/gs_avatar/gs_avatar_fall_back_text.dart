4edd02f1959efeb4554d58dfecac1477650d4ee9
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-01 12:33:50 +0530

    resolved deprecated prop in gs avatar txt

--- lib/src/widgets/gs_avatar/gs_avatar_fall_back_text.dart
+++ lib/src/widgets/gs_avatar/gs_avatar_fall_back_text.dart
@@ -26,7 +26,7 @@ class GSAvatarFallBackText extends StatelessWidget {
   final TextAlign? textAlign;
   final TextDirection? textDirection;
   final TextHeightBehavior? textHeightBehavior;
-  final double? textScaleFactor;
+  final TextScaler? textScaler;
   final TextWidthBasis? textWidthBasis;
 
   /// Constructor for GSAvatarFallBackText widget:
@@ -45,7 +45,7 @@ class GSAvatarFallBackText extends StatelessWidget {
       this.textAlign,
       this.textDirection,
       this.textHeightBehavior,
-      this.textScaleFactor,
+      this.textScaler,
       this.textWidthBasis,
       this.bold = false,
       this.highlight = false,
@@ -105,7 +105,7 @@ class GSAvatarFallBackText extends StatelessWidget {
       textAlign: textAlign,
       textDirection: textDirection,
       textHeightBehavior: textHeightBehavior,
-      textScaleFactor: textScaleFactor,
+      textScaler: textScaler,
       textWidthBasis: textWidthBasis,
     );
   }
