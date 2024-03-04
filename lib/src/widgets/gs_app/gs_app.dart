3730c0aac665097d2a07d0383ab63c7c9900c5d3
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-04 12:42:26 +0530

    fix/gs-app-localization

--- lib/src/widgets/gs_app/gs_app.dart
+++ lib/src/widgets/gs_app/gs_app.dart
@@ -25,7 +25,11 @@ class GSApp extends StatefulWidget {
     this.onGenerateTitle,
     this.color,
     this.locale,
-    this.localizationsDelegates,
+    this.localizationsDelegates = const [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ],
     this.localeListResolutionCallback,
     this.localeResolutionCallback,
     this.supportedLocales = const <Locale>[Locale('en', 'US')],
@@ -165,8 +169,8 @@ class _GSAppState extends State<GSApp> {
       yield* localizationsDelegates;
     }
     yield GlobalMaterialLocalizations.delegate;
-    yield GlobalCupertinoLocalizations.delegate;
     yield GlobalWidgetsLocalizations.delegate;
+    yield GlobalCupertinoLocalizations.delegate;
   }
 
   bool get _usesRouter =>
@@ -259,7 +263,7 @@ class _GSAppState extends State<GSApp> {
       title: widget.title,
       onGenerateTitle: widget.onGenerateTitle,
       locale: widget.locale,
-      localizationsDelegates: widget.localizationsDelegates,
+      localizationsDelegates: _localizationsDelegates,
       localeListResolutionCallback: widget.localeListResolutionCallback,
       localeResolutionCallback: widget.localeResolutionCallback,
       supportedLocales: widget.supportedLocales,
