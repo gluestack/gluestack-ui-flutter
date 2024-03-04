3bce39ad295420ec41cd1ac695a5c5b943d9a4ce
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-04 18:23:54 +0530

    added comments to icon button

--- lib/src/widgets/gs_icon_button/gs_btn_icon.dart
+++ lib/src/widgets/gs_icon_button/gs_btn_icon.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gluestack_ui/gluestack_ui.dart';
 
+/// predefined sizes for [GSIconButton], providing a consistent set of size options for icon buttons.
 enum GSIconButtonSizes {
   $xs,
   $sm,
@@ -9,15 +10,32 @@ enum GSIconButtonSizes {
   $lg,
 }
 
+/// A widget that represents an icon button,
+/// [GSIconButton] allows for the creation of buttons with icon content, supporting various interactions
+/// such as taps, long presses, and double taps.
 class GSIconButton extends StatelessWidget {
+  /// The icon to display within the button.
   final GSIcon icon;
+
+  /// The callback that is called when the button is tapped.
   final VoidCallback onPressed;
+
+  /// The callback that is called when the button is long-pressed.
   final VoidCallback? onLongPress;
+
+  /// The callback that is called when the button is double-tapped.
   final GestureDoubleTapCallback? onDoubleTap;
+
+  /// An optional semantic label for the button, used by screen readers.
   final String? semanticsLabel;
+
+  /// The size of the icon button, affecting its overall dimensions.
   final GSIconButtonSizes? size;
 
+  /// Custom [GSStyle] to apply to the button, enabling detailed customization of its appearance.
   final GSStyle? style;
+
+  ///Constructor for [GSIconButton]
   const GSIconButton({
     super.key,
     required this.icon,
