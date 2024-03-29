1a2a7926314cc24a787cd965131b010c2bbe7a56
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-29 17:53:03 +0530

    ex improvement

--- example/lib/widgets/components/widgets/header_example.dart
+++ example/lib/widgets/components/widgets/header_example.dart
@@ -59,14 +59,14 @@ class HeaderExample extends StatelessWidget {
           body: GSVStack(
             children: [
               const GSHeader(
-                // centerChild: true,
+                centerChild: true,
                 child: GSText(
                   text: 'GS Header Sample',
                 ),
-                trailingWidget: GSText(
-                  text: 'TRAIL',
-                ),
-                leadingWidget: Icon(Icons.architecture),
+                // trailingWidget: GSText(
+                //   text: 'TRAIL',
+                // ),
+                // leadingWidget: Icon(Icons.architecture),
               ),
               GSBox(
                 style: GSStyle(
