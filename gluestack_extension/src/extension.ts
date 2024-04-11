1fe428cb67c9585af13538d45c4e7277eae2d48c
ashishg <ashishg@geekyants.com>
2024-04-11 18:06:10 +0530

    feat: VSCode extension added for gluestack-ui-flutter

--- gluestack_extension/src/extension.ts
+++ gluestack_extension/src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { GSCodeActionProvider } from "./gscode_extension_provider";
 
 export function activate(context: vscode.ExtensionContext) {
   // Command to wrap with GSBox
@@ -151,51 +152,4 @@ function adjustImportsForGSBox(document: vscode.TextDocument) {}
 
 function adjustImports(document: vscode.TextDocument) {
   // Logic to adjust imports
-}
-
-class GSCodeActionProvider implements vscode.CodeActionProvider {
-  provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range,
-    context: vscode.CodeActionContext,
-    token: vscode.CancellationToken
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    // Suggestions for code actions
-    const actions: vscode.CodeAction[] = [];
-
-    const wrapWithGSBoxAction = new vscode.CodeAction(
-      "Wrap with GSBox",
-      vscode.CodeActionKind.QuickFix
-    );
-    wrapWithGSBoxAction.command = {
-      title: "Wrap with GSBox",
-      command: "extension.wrapWithGSBox",
-      arguments: [document, range],
-    };
-    actions.push(wrapWithGSBoxAction);
-
-    const wrapWithGSHStackAction = new vscode.CodeAction(
-      "Wrap with GSHStack",
-      vscode.CodeActionKind.QuickFix
-    );
-    wrapWithGSHStackAction.command = {
-      title: "Wrap with GSHStack",
-      command: "extension.wrapWithGSHStack",
-      arguments: [document, range],
-    };
-    actions.push(wrapWithGSHStackAction);
-
-    const wrapWithGSVStackAction = new vscode.CodeAction(
-      "Wrap with GSVStack",
-      vscode.CodeActionKind.QuickFix
-    );
-    wrapWithGSVStackAction.command = {
-      title: "Wrap with GSVStack",
-      command: "extension.wrapWithGSVStack",
-      arguments: [document, range],
-    };
-    actions.push(wrapWithGSVStackAction);
-
-    return actions;
-  }
 }
