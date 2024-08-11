8533b77c10cdd47241d48b5027a49cf877c4d8eb
singlapriyanka315 <priyanka0363.cse19@chitkara.edu.in>
2024-08-11 21:04:04 +0530

    remove overrides from slider paints

--- lib/src/widgets/gs_slider/gs_slider_painter.dart
+++ lib/src/widgets/gs_slider/gs_slider_painter.dart
@@ -159,13 +159,4 @@ class SliderPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
-
-  @override
-  bool? hitTest(Offset position) => null;
-
-  @override
-  SemanticsBuilderCallback? get semanticsBuilder => null;
-
-  @override
-  bool shouldRebuildSemantics(covariant CustomPainter oldDelegate) => false;
 }
