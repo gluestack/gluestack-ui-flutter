97c293bd1a0a1d695a0e02d5a284292c9dec6174
singlapriyanka315 <priyanka0363.cse19@chitkara.edu.in>
2024-08-12 17:36:04 +0530

    fix radio_raw file export

--- lib/src/widgets/gs_radio/gs_radio_raw.dart
+++ lib/src/widgets/gs_radio/gs_radio_raw.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:gluestack_ui/gluestack_ui.dart';
-import 'gs_toggleable.dart' as custom_toggleable;
 
 const Size _size = Size(18.0, 18.0);
 const double _kOuterRadius = 8.0;
@@ -59,7 +58,7 @@ class GSRawRadio<T> extends StatefulWidget {
 }
 
 class _GSRawRadioState<T> extends State<GSRawRadio<T>>
-    with TickerProviderStateMixin, custom_toggleable.ToggleableStateMixin {
+    with TickerProviderStateMixin, ToggleableStateMixin {
   bool focused = false;
 
   void _handleChanged(bool? selected) {
@@ -150,7 +149,7 @@ class _GSRawRadioState<T> extends State<GSRawRadio<T>>
   }
 }
 
-class _RadioPainter extends custom_toggleable.ToggleablePainter {
+class _RadioPainter extends ToggleablePainter {
   final double? radioSize;
 
   _RadioPainter({
